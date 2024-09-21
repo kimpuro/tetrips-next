17fe663ecf6d5f0ee5811dc9b743e5605a1a690a
KimPuro <puro@protonmail.ch>
2024-09-21 21:51:59 +0900

    feat : next auth에 구글 provider 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,16 @@
-import { handlers } from "@/auth" // Referring to the auth.ts we just created
-export const { GET, POST } = handlers
+import Google from "next-auth/providers/google"
+import NextAuth from 'next-auth'
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Google({
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+})
